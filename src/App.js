7d656b73a4f14075b5679ff5056d130562fa1b92
Edgard Gamboa <Edgard.Gamboa@imail.org>
2022-12-01 14:10:54 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Nav from './Nav/Nav.js'
+import Home from './Home/Home.js'
+import Header from './Home/Header.js'
+
 
 function App() {
-  return <div>Hello World</div>;
+  return(
+    <>
+    <Header />
+    <Home />
+    </>
+  )
 }
 
 export default App;
+
+
---
+++ src/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css'
+import SearchIcon from '@mui/icons-material/Search';
+function Nav() {
+  return (
+    <div className='nav'>
+        <h1>Hello</h1>
+        <div className='nav_left'>
+            <image src ="" alt = ""/>
+            <div className='nav_search'></div>
+            <div className='icons'></div>
+        </div>
+        <div className='nav_right'></div>
+      
+    </div>
+  )
+}
+
+export default Nav
