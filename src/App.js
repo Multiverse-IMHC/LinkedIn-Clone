e732510b0dfa158e56f3b0d23c82be2e207ad539
shayNicol <shaynicolaslove@gmail.com>
2022-12-08 14:23:17 -0500

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Home from "./Home/Home"
 import Header from "./Home/Header"
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import SignInPage from "./components/Login/SignInPage";
 
 function App() {
-  return <div>
-    <Header/>
-    <Home/>
-  </div>;
+  return( <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path = "/" element= {<SignInPage/>}/>
+    <Route path = "/home" element= {<div><Header/><Home/></div>
+}/>
+    </Routes>
+    </BrowserRouter>
+  </div>
+  )
 }
 
 export default App;
