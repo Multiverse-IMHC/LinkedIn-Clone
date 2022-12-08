2724506571b6521fabfb861b70135dcd02d31134
WilliamVanterpool <williamvanterpool03@gmail.com>
2022-12-08 16:08:48 -0500

    added user test model

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,34 @@ const {Sequelize} = require('sequelize');
 const {sequelize} = require('../db');
 
 const Post = sequelize.define('Post', {
+    postId: {type: Sequelize.INTEGER,
+        autoIncrement: true, primaryKey:true},
     name: Sequelize.STRING,
     text: Sequelize.STRING,
 });
 
+const User = sequelize.define('User', {
+    id: {type: Sequelize.INTEGER,
+        autoIncrement: true, primaryKey:true},
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+    password2: Sequelize.STRING
+})
+
+User.associate = models => {
+  User.hasMany(models.Post, {
+    foreignKey: 'posterId'
+  });
+}
+
+Post.associate = (models) => {
+    Post.belongsTo(models.User, {
+      foreignKey: 'posterId'
+    })
+  }
+
+
 module.exports = Post;
+module.exports = User;
